83ec1f0f89e3a53920835bda7e2a32bf6134d4a0
hassan <hassan.elouardy06@gmail.com>
2023-07-04 17:00:59 +0100

    Add styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
 const pizzaData = [
   {
     name: "Focaccia",
@@ -47,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -56,18 +58,24 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza co.</h1>;
+  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
-    </div>
+    </main>
   );
 }
 
@@ -82,14 +90,16 @@ function Footer() {
   // else alert("We're currently close!");
 
   return (
-    <footer>{new Date().toLocaleDateString()} We're currently open</footer>
+    <footer className="footer">
+      {new Date().toLocaleDateString()} We're currently open
+    </footer>
   );
 }
 
 function Pizza() {
   return (
     <div>
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
       <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci<" />
     </div>
@@ -103,11 +113,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// React 18
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
